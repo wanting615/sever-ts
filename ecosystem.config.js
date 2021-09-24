abeea3e428ff00d106b2581ebb6268fd85f12465
wanting <65777214@qq.com>
2021-09-24 16:15:59 +0800

    pm2配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,32 @@
 module.exports = {
   apps: [{
-    script: 'index.js',
-    watch: './dist'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
+    name: 'elm-server',
+    script: './dist/server.js',
+    watch: './dist',
+    error_file: "./logs/err.log",//错误输出日志
+    out_file: "./logs/out.log",  //日志
+    log_date_format: "YYYY-MM-DD HH:mm Z", //日期格式
+    env_production: {
+      "NODE_ENV": "production" // 环境变量
+    }
   }],
 
   deploy: {
     production: {
       user: 'root',
-      host: '121.5.32.253',
-      port: '22',
+      host: [{
+        host: '121.5.32.253',
+        port: '22'
+      }],
       ref: 'origin/master',
-      repo: 'GIT_REPOSITORY',
-      path: 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+      repo: 'git@github.com:wanting615/sever-ts.git',
+      path: '/www/elm/',
+      env: {
+        "NODE_ENV": "production"
+      },
+      ssh_options: "StrictHostKeyChecking=no",
+      'pre-deploy-local': "echo '生产环境部署中'",
+      'post-deploy': 'npm install && npm run build-ts && pm2 restart ecosystem.config.js --env production',
     }
   }
 };
